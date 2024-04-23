7e6a131f8244387f0dd58b7ceb4f429e147e10e9
kschri201_comcast <kurt_schriefer@cable.comcast.com>
2024-04-23 10:59:45 -0400

    Remove Cypress explanation

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,21 +16,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// ***********************************************************
-// This example support/e2e.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
 // Importing generic module
 import * as genericModule from './cypress-support/src/main';
 import * as configModule from 'configModule';
