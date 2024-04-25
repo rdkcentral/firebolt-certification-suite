6ef4cb9cbe8cbd5653f52ea39d201856097fcc5b
SnehalP251 <snehal89.patil@gmail.com>
2024-04-25 23:51:37 +0530

    Logs Update

--- cypress/support/cypress-commands/lifecycle.js
+++ cypress/support/cypress-commands/lifecycle.js
@@ -518,17 +518,15 @@ function validateVisibilityState(state) {
         const pretext =
           CONSTANTS.VISIBILITYSTATE_VALIDATION_REQ + lifecycleStateRequirementId.visible_check.id;
         if (visibilityState[state] != result.report) {
-          const fixLog = eval(CONSTANTS.VISIBILITYSTATE_FAILURE_FIX_LOG);
-          const assertLog = eval(CONSTANTS.VISIBILITYSTATE_FAILURE_LOG);
           cy.log(
             pretext +
               ': Expected : ' +
               visibilityState[state] +
               ' , Actual : ' +
               result.report +
-              fixLog
+              CONSTANTS.VISIBILITYSTATE_FAILURE_FIX_LOG
           ).then(() => {
-            assert.equal(visibilityState[state], result.report + assertLog);
+            assert.equal(visibilityState[state], result.report + CONSTANTS.VISIBILITYSTATE_FAILURE_LOG);
           });
         }
         cy.log(
@@ -539,6 +537,6 @@ function validateVisibilityState(state) {
       });
     }
   } else {
-    cy.log(eval(CONSTANTS.LIFECYCLE_VISIBILITYSTATE_SKIP_MESSAGE));
+    cy.log(CONSTANTS.LIFECYCLE_VISIBILITYSTATE_SKIP_MESSAGE);
   }
 }
