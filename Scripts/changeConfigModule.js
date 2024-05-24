04e9a7018da2052e6a6e3ae691a9ff7f21649a0b
Kandasamy, Divya <kdivya153@gmail.com>
2024-05-24 12:24:42 -0400

    Update changeConfigModule.js

--- Scripts/changeConfigModule.js
+++ Scripts/changeConfigModule.js
@@ -51,7 +51,7 @@ function addOrChangeConfigModule(configModuleUrl) {
 // argv[0] is "node". argv[1] is the path to this file
 if (process.argv.length != 3) {
   console.log(
-    'Invalid format. Required script formats: "node ./changeConfigModule.js <configModuleUrl>"\n' +
+    'Invalid format. Required script format: "node ./changeConfigModule.js <configModuleUrl>"\n' +
       'Ex: "node ./changeConfigModule.js git//git@github.com/myOrg/myConfigModule.git"'
   );
   process.exit(1);
