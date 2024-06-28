7a4e3ba438bf39165623e649d2897a6c3c280d51
Abhishek urs C J <abhishekurst@gmail.com>
2024-06-28 15:15:50 +0530

    Reverted intent changes

--- cypress/support/cypress-commands/commands.js
+++ cypress/support/cypress-commands/commands.js
@@ -694,10 +694,7 @@ Cypress.Commands.add('launchApp', (appType, appCallSign) => {
         },
       },
     };
-    const messageIntent = {
-      data: data,
-    };
-    requestMap.params.intent = messageIntent;
+    requestMap.params.intent.data = data;
   }
 
   // Add the PubSub URL if required
