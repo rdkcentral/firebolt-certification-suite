ebbb9e9782ce1fdd3647747dbdf369a70ce2bd51
preethi.m <preethi.m4@tataelxsi.co.in>
2024-07-01 21:26:26 +0530

    lint errors

--- cypress/fixtures/fireboltCalls/accessibility.js
+++ cypress/fixtures/fireboltCalls/accessibility.js
@@ -1,81 +1,90 @@
 exports.CLOSEDCAPTION_SETTINGS = {
-    method: "accessibility.closedCaptionsSettings",
-    params: null,
-    validationJsonPath: resolveAtRuntime(["result.{{attribute}}", "result.styles.{{attribute}}"]),
-    setMethod: resolveAtRuntime("manage_closedcaptions.set{{attribute.uppercaseFirstChar}}"),
-    setParams: resolveAtRuntime("value"),
-    setValidationJsonPath: "result",
-    setContent: null,
-    event: "accessibility.onClosedCaptionsSettingsChanged",
-    eventValidationJsonPath: resolveAtRuntime(["eventResponse.{{attribute}}", "eventResponse.styles.{{attribute}}"]),
-    content: {
-        data: [
-            {
-                type: "fixture",
-                validations: [
-                    {
-                        mode: "staticContentValidation",
-                        type: resolveAtRuntime("value"),
-                        description: resolveAtRuntime("Validating that accessibility.closedCaptionSettings {{attribute}} is {{value}}")
-                    }
-                ]
-            }
-        ]
-    }
+  method: 'accessibility.closedCaptionsSettings',
+  params: null,
+  validationJsonPath: resolveAtRuntime(['result.{{attribute}}', 'result.styles.{{attribute}}']),
+  setMethod: resolveAtRuntime('manage_closedcaptions.set{{attribute.uppercaseFirstChar}}'),
+  setParams: resolveAtRuntime('value'),
+  setValidationJsonPath: 'result',
+  setContent: null,
+  event: 'accessibility.onClosedCaptionsSettingsChanged',
+  eventValidationJsonPath: resolveAtRuntime([
+    'eventResponse.{{attribute}}',
+    'eventResponse.styles.{{attribute}}',
+  ]),
+  content: {
+    data: [
+      {
+        type: 'fixture',
+        validations: [
+          {
+            mode: 'staticContentValidation',
+            type: resolveAtRuntime('value'),
+            description: resolveAtRuntime(
+              'Validating that accessibility.closedCaptionSettings {{attribute}} is {{value}}'
+            ),
+          },
+        ],
+      },
+    ],
+  },
 };
 
 exports.VOICEGUIDANCE_SETTINGS = () => {
-    return {
-    method: "accessibility.voiceGuidanceSettings",
+  return {
+    method: 'accessibility.voiceGuidanceSettings',
     params: null,
-    validationJsonPath: resolveAtRuntime(["result.{{attribute}}"]),
-    setMethod: resolveAtRuntime("manage_voiceguidance.set{{attribute.uppercaseFirstChar}}"),
-    setParams: resolveAtRuntime("value"),
-    setValidationJsonPath: "result",
+    validationJsonPath: resolveAtRuntime(['result.{{attribute}}']),
+    setMethod: resolveAtRuntime('manage_voiceguidance.set{{attribute.uppercaseFirstChar}}'),
+    setParams: resolveAtRuntime('value'),
+    setValidationJsonPath: 'result',
     setContent: null,
-    event: "accessibility.onVoiceGuidanceSettingsChanged",
-    eventValidationJsonPath: resolveAtRuntime(["eventResponse.{{attribute}}"]),
+    event: 'accessibility.onVoiceGuidanceSettingsChanged',
+    eventValidationJsonPath: resolveAtRuntime(['eventResponse.{{attribute}}']),
     content: {
-        data: [
+      data: [
+        {
+          type: 'fixture',
+          validations: [
             {
-                type: "fixture",
-                validations: [
-                    {
-                        mode: "staticContentValidation",
-                        type: resolveAtRuntime("value"),
-                        description: resolveAtRuntime("Validating that accessibility.voiceGuidanceSettings {{attribute}} is {{value}}")
-                    }
-                ]
-            }
-        ]
-    }
-}
+              mode: 'staticContentValidation',
+              type: resolveAtRuntime('value'),
+              description: resolveAtRuntime(
+                'Validating that accessibility.voiceGuidanceSettings {{attribute}} is {{value}}'
+              ),
+            },
+          ],
+        },
+      ],
+    },
+  };
 };
 
 exports.AUDIODESCRIPTIONS_SETTINGS = () => {
-    return {
-    method: "accessibility.audioDescriptionSettings",
+  return {
+    method: 'accessibility.audioDescriptionSettings',
     params: null,
-    validationJsonPath: resolveAtRuntime(["result.{{attribute}}"]),
-    setMethod: resolveAtRuntime("manage_audiodescriptions.set{{attribute.uppercaseFirstChar}}"),
-    setParams: resolveAtRuntime("value"),
-    setValidationJsonPath: "result",
+    validationJsonPath: resolveAtRuntime(['result.{{attribute}}']),
+    setMethod: resolveAtRuntime('manage_audiodescriptions.set{{attribute.uppercaseFirstChar}}'),
+    setParams: resolveAtRuntime('value'),
+    setValidationJsonPath: 'result',
     setContent: null,
-    event: "accessibility.onAudioDescriptionSettingsChanged",
-    eventValidationJsonPath: resolveAtRuntime(["eventResponse.{{attribute}}"]),
+    event: 'accessibility.onAudioDescriptionSettingsChanged',
+    eventValidationJsonPath: resolveAtRuntime(['eventResponse.{{attribute}}']),
     content: {
-        data: [
+      data: [
+        {
+          type: 'fixture',
+          validations: [
             {
-                type: "fixture",
-                validations: [
-                    {
-                        mode: "staticContentValidation",
-                        type: resolveAtRuntime("value"),
-                        description: resolveAtRuntime("Validating that accessibility.audioDescriptionSettings {{attribute}} is {{value}}")
-                    }
-                ]
-            }
-        ]
-    }
-}
+              mode: 'staticContentValidation',
+              type: resolveAtRuntime('value'),
+              description: resolveAtRuntime(
+                'Validating that accessibility.audioDescriptionSettings {{attribute}} is {{value}}'
+              ),
+            },
+          ],
+        },
+      ],
+    },
+  };
 };
