3c7dd767424486b5b6c8a51b2814078b802ec66c
anjalimukundan <anjalimukundan13@gmail.com>
2024-07-19 11:50:03 +0530

    removed the IntegratedPlayer module related files

--- cypress/support/step_definitions/fireboltCalls.js
+++ cypress/support/step_definitions/fireboltCalls.js
@@ -82,6 +82,12 @@ Given(/1st party app invokes the (?:'(.+)' )?API to '(.+)'$/, async (sdk, key) =
             if (UTILS.isScenarioExempted(method, params)) {
               Cypress.env(CONSTANTS.IS_SCENARIO_EXEMPTED, true);
             }
+            // If testType integratedPlayer save playerId to env variable if exists
+            if (Cypress.env(CONSTANTS.TEST_TYPE).toLowerCase() == 'integratedplayer') {
+              if (updatedResponse && updatedResponse.player && updatedResponse.player.playerId) {
+                Cypress.env('PlayerId', updatedResponse.player.playerId);
+              }
+            }
             // Creating object with event name, params, and response etc and storing it in a global list for further validation.
             const apiAppObject = new apiObject(
               method,
