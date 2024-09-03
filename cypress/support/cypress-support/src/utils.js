465ce9e523c78c4114cc81a09d1b9ce6f4fe555b
Joe Meier <joseph_meier@comcast.com>
2024-09-03 12:31:55 -0400

    Linting

--- cypress/support/cypress-support/src/utils.js
+++ cypress/support/cypress-support/src/utils.js
@@ -235,7 +235,7 @@ function getCommunicationMode() {
  * skipCurrentTest()
  */
 function skipCurrentTest() {
-  fireLog.info("The current test has been intentionally skipped by the test runner")
+  fireLog.info('The current test has been intentionally skipped by the test runner')
   mocha.suite.ctx.currentTest.skip();
 }
 
@@ -1098,5 +1098,5 @@ module.exports = {
   resolveRecursiveValues,
   fireboltCallObjectHasField,
   fetchAppIdentifierFromEnv,
-  skipCurrentTest
+  skipCurrentTest,
 };
