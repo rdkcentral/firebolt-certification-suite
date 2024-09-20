d90735112c28be0e1e6cb611c8bb159567e620e9
Abhishek urs C J <abhishekurst@gmail.com>
2024-09-20 16:51:29 +0530

    Added changes to handle empty validations

--- cypress/support/validations/screenshotValidation.js
+++ cypress/support/validations/screenshotValidation.js
@@ -7,12 +7,11 @@ const UTILS = require('../cypress-support/src/utils');
  * @description To validate the content of the response if undefined
  * @param {Object} object - The object to validate
  * @example
- * cy.screenshotValidation()
+ * cy.screenshotValidation({})
  */
 Cypress.Commands.add('screenshotValidation', (object) => {
   if (UTILS.getEnvVariable('enableScreenshots')) {
-    console.log('object', object);
-    if (object && object.validations && object.validations.length > 0) {
+    if (object && object.validations) {
       const requestMap = {
         method: 'fcs.screenshot',
         params: {
@@ -21,11 +20,7 @@ Cypress.Commands.add('screenshotValidation', (object) => {
         },
       };
 
-      cy.sendMessagetoPlatforms(requestMap).then((response) => {
-        cy.log('Screenshot validation response: ', response).then(() => {
-          fireLog.equal('pass', response.status, 'Screenshot validation status');
-        });
-      });
+      cy.sendMessagetoPlatforms(requestMap)
     } else {
       fireLog.fail('No validations found for screenshot.');
     }
