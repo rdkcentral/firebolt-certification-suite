6ccd34c2ddd206f6f0f85d28155d42ea091340dd
Abhishek urs C J <abhishekurst@gmail.com>
2024-10-17 10:02:05 +0530

    Added minor change

--- cypress/support/constants/constants.js
+++ cypress/support/constants/constants.js
@@ -356,6 +356,7 @@ module.exports = {
     CREATE_MARKER: 'performance.createMarker',
     UNLOADAPP: 'fcs.unloadApp',
     FETCHDEVICEDETAILS: 'fcs.fetchDeviceDetails',
+    SCREENSHOT: 'fcs.screenshot',
   },
   REQUEST_MAP_INTERACTIONS_SERVICE: 'Request map for firebolt interactions service : ',
   RESPONSE: 'Response: ',
