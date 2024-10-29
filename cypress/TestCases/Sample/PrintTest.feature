71b04de3b7e48535f67a281ce5fae2ecc766d9ce
kschrief <kschriefer91@gmail.com>
2024-10-29 14:38:16 -0400

    NOJIRA: Add sample printVar test case

---
+++ cypress/TestCases/Sample/PrintTest.feature
@@ -0,0 +1,3 @@
+Given I print the 'firstPartyAppId' environment variable
+Given I print the 'deviceIp' environment variable
+Given I print the 'platformCommunication' environment variable
--- cypress/support/step_definitions/sample.js
+++ cypress/support/step_definitions/sample.js
@@ -18,6 +18,10 @@
 import { Given } from '@badeball/cypress-cucumber-preprocessor';
 const logger = require('../Logger')('sample.js');
 
+Given('I print the {string} environment variable', (echo) => {
+  logger.info("The value for Cypress.env(\"" + echo + "\") is " + Cypress.env("echo"));
+});
+
 Given('I say {string}', (echo) => {
   logger.info(`You said ${echo}`);
   cy.log(`You said ${echo}!`);
