0b2ad476a86f1469012d508114afe59e153148c8
kschrief <kschriefer91@gmail.com>
2024-10-29 14:55:06 -0400

    NOJIRA: Add sample printVar test case

--- cypress/support/step_definitions/sample.js
+++ cypress/support/step_definitions/sample.js
@@ -19,7 +19,7 @@ import { Given } from '@badeball/cypress-cucumber-preprocessor';
 const logger = require('../Logger')('sample.js');
 
 Given('I print the {string} environment variable', (echo) => {
-  logger.info("The value for Cypress.env(\"" + echo + "\") is " + Cypress.env("echo"));
+  logger.info("The value for Cypress.env(\"" + echo + "\") is " + Cypress.env(echo));
 });
 
 Given('I say {string}', (echo) => {
