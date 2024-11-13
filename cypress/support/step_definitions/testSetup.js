25f1f0d6407ee17341982fff85dd6432b4dc64d0
Joe Meier <joseph_meier@comcast.com>
2024-11-13 15:16:07 -0500

    Add additional or statements to catch unloading only tests

--- cypress/support/step_definitions/testSetup.js
+++ cypress/support/step_definitions/testSetup.js
@@ -37,7 +37,7 @@ Given('the environment has been set up for {string} tests', (test) => {
   }
   if (
     !UTILS.getEnvVariable(CONSTANTS.ENV_SETUP_STATUS, false) ||
-    UTILS.getEnvVariable(CONSTANTS.LIFECYCLE_CLOSE_TEST_TYPES).includes(test) ||
+    UTILS.getEnvVariable(CONSTANTS.LIFECYCLE_CLOSE_TEST_TYPES).includes(test) || UTILS.getEnvVariable(CONSTANTS.UNLOADING_APP_TEST_TYPES).includes(test) ||
     UTILS.isTestTypeChanged(test)
   ) {
     Cypress.env(CONSTANTS.PREVIOUS_TEST_TYPE, Cypress.env(CONSTANTS.TEST_TYPE));
