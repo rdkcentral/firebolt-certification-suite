a025108c67fd945f76ddf396d961e7558ae170be
Keaton Sentak <ksentak01@gmail.com>
2024-11-13 09:03:07 -0500

    feat: Add screenshot btn on local cuke reports

--- Scripts/copyCukeReportTemplate.js
+++ Scripts/copyCukeReportTemplate.js
@@ -15,29 +15,63 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+/**
+ * @description This script is resposible for copying specific report template files
+ * from a custom template directory to the `multiple-cucumber-html-reporter` node module.
+ * This ensures that the custom report templates are used when generating cucumber reports.
+ */
 const fs = require('fs');
 const path = require('path');
 const logger = require('../cypress/support/Logger')('copyCukeReportTemplate.js');
-const SOURCE_REPORT_TEMPLATE = path.join(__dirname, '..', 'reportTemplates', 'scenarios.tmpl');
-const DEST_REPORT_TEMPLATE = path.join(
-  __dirname,
-  '..',
-  'node_modules',
-  'multiple-cucumber-html-reporter',
-  'templates',
-  'components',
-  'scenarios.tmpl'
-);
 
-// Function to copy file with error handling
+// Define the array of template files to be copied with source and destination paths
+const FILES_TO_COPY = [
+  {
+    source: path.join(__dirname, '..', 'reportTemplates', 'scenarios.tmpl'),
+    destination: path.join(
+      __dirname,
+      '..',
+      'node_modules',
+      'multiple-cucumber-html-reporter',
+      'templates',
+      'components',
+      'scenarios.tmpl'
+    ),
+  },
+  {
+    source: path.join(__dirname, '..', 'reportTemplates', 'generic.js'),
+    destination: path.join(
+      __dirname,
+      '..',
+      'node_modules',
+      'multiple-cucumber-html-reporter',
+      'templates',
+      'generic.js'
+    ),
+  },
+];
+
+/**
+ * @function copyFileIfExist
+ * @description Copies a file from the source path to the destination path if both paths exist.
+ * Logs an informational message on success or an error message if the file is missing or
+ * if the operation fails.
+ *
+ * @param {string} source - The file path to copy from.
+ * @param {string} destination - The file path to copy to.
+ */
 function copyFileIfExist(source, destination) {
   try {
+    // Check if the source file exists
     if (!fs.existsSync(source)) {
       throw new Error(`Report template in FCS: "${source}" does not exist.`);
     }
+    // Check if the destination file exists
     if (!fs.existsSync(destination)) {
       throw new Error(`Report template in nodeModules: "${destination}" does not exist.`);
     }
+    // Copy the source file to the destination
     fs.copyFileSync(source, destination);
     logger.info(`Copied  Report template from "${source}" to "${destination}"`);
   } catch (error) {
@@ -45,5 +79,7 @@ function copyFileIfExist(source, destination) {
   }
 }
 
-// Copy report template file
-copyFileIfExist(SOURCE_REPORT_TEMPLATE, DEST_REPORT_TEMPLATE);
+// Iterate through each file in FILES_TO_COPY and perform the copy operation
+FILES_TO_COPY.forEach((file) => {
+  copyFileIfExist(file.source, file.destination);
+});
