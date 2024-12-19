684f57e979d208efa640ae24ed9c03e45d006077
Kandasamy, Divya <kdivya153@gmail.com>
2024-12-19 14:24:10 -0500

    template for fcsSetters

---
+++ defaultModule/requestModules/fcsSetters.js
@@ -0,0 +1,93 @@
+const setClosedCaptions = async (attribute, value) => {
+  return setterNotImplemented();
+};
+
+const setAudioDescriptionSettings = async (attribute, value) => {
+  return setterNotImplemented();
+};
+
+const setVoiceGuidance = async (attribute, value) => {
+  return setterNotImplemented();
+};
+
+const setLimitAdTracking = async (value) => {
+  return setterNotImplemented();
+};
+
+const setVideoResolution = async (value) => {
+  return setterNotImplemented();
+};
+
+const setDeviceAudio = async (value) => {
+  return setterNotImplemented();
+};
+
+const launchApp = async (value) => {
+  return setterNotImplemented();
+};
+
+const setDiscoveryPolicy = async (value) => {
+  return setterNotImplemented();
+};
+
+const setLifecycleState = async (attribute, value) => {
+  return setterNotImplemented();
+};
+
+const unloadApp = async (value) => {
+  return setterNotImplemented();
+};
+
+const setLanguage = async (value) => {
+  return setterNotImplemented();
+};
+
+const setPreferredAudioLanguages = async (value) => {
+  return setterNotImplemented();
+};
+
+const setLocale = async (value) => {
+  return setterNotImplemented();
+};
+
+const setAdditionalInfo = async (value) => {
+  return setterNotImplemented();
+};
+
+const setDeviceHdr = async (value) => {
+  return setterNotImplemented();
+};
+
+const setDeviceHdcp = async (value) => {
+  return setterNotImplemented();
+};
+
+const setDeviceNetwork = async (value) => {
+  return setterNotImplemented();
+};
+
+const setCountryCode = async (value) => {
+  return setterNotImplemented();
+};
+
+const fcsSetters = {
+  setClosedCaptions,
+  setAudioDescriptionSettings,
+  setVoiceGuidance,
+  setLimitAdTracking,
+  setDeviceHdr,
+  setDeviceHdcp,
+  setDeviceNetwork,
+  setVideoResolution,
+  setDeviceAudio,
+  launchApp,
+  setDiscoveryPolicy,
+  setLifecycleState,
+  unloadApp,
+  setCountryCode,
+  setLanguage,
+  setPreferredAudioLanguages,
+  setLocale,
+  setAdditionalInfo,
+};
+module.exports = fcsSetters;
