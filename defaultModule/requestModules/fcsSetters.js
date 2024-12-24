78f34e8c82ed8b322acd049a4beded9f5eae634b
Kandasamy, Divya <kdivya153@gmail.com>
2024-12-24 13:13:28 -0500

    Update fcsSetters.js

--- defaultModule/requestModules/fcsSetters.js
+++ defaultModule/requestModules/fcsSetters.js
@@ -1,93 +1,223 @@
-const setClosedCaptions = async (attribute, value) => {
-  return setterNotImplemented();
-};
-
-const setAudioDescriptionSettings = async (attribute, value) => {
-  return setterNotImplemented();
-};
-
-const setVoiceGuidance = async (attribute, value) => {
-  return setterNotImplemented();
-};
-
-const setLimitAdTracking = async (value) => {
-  return setterNotImplemented();
-};
-
-const setVideoResolution = async (value) => {
-  return setterNotImplemented();
-};
-
-const setDeviceAudio = async (value) => {
-  return setterNotImplemented();
-};
-
+/**
+ * Launches an application.
+ * @param {any} value - The application to launch.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.launchApp("AppName");
+ */
 const launchApp = async (value) => {
   return setterNotImplemented();
 };
 
-const setDiscoveryPolicy = async (value) => {
-  return setterNotImplemented();
-};
+// TODO: Check whether it needs both attribute and value
 
-const setLifecycleState = async (attribute, value) => {
-  return setterNotImplemented();
-};
-
-const unloadApp = async (value) => {
-  return setterNotImplemented();
-};
-
-const setLanguage = async (value) => {
-  return setterNotImplemented();
-};
-
-const setPreferredAudioLanguages = async (value) => {
-  return setterNotImplemented();
-};
-
-const setLocale = async (value) => {
-  return setterNotImplemented();
-};
-
+/**
+ * Sets additional metadata for localization or configuration.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setAdditionalInfo("{ key: 'exampleKey', value: 'exampleValue' }");
+ */
 const setAdditionalInfo = async (value) => {
   return setterNotImplemented();
 };
 
-const setDeviceHdr = async (value) => {
+/**
+ * Sets audio description settings.
+ * @param {string} attribute - The attribute to set.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setAudioDescriptionSettings("setEnabled", "true");
+ */
+const setAudioDescriptionSettings = async (attribute, value) => {
   return setterNotImplemented();
 };
 
-const setDeviceHdcp = async (value) => {
-  return setterNotImplemented();
-};
-
-const setDeviceNetwork = async (value) => {
-  return setterNotImplemented();
-};
-
+/**
+ * Sets the country code.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setCountryCode("US");
+ */
 const setCountryCode = async (value) => {
   return setterNotImplemented();
 };
 
-const fcsSetters = {
-  setClosedCaptions,
-  setAudioDescriptionSettings,
-  setVoiceGuidance,
-  setLimitAdTracking,
-  setDeviceHdr,
-  setDeviceHdcp,
-  setDeviceNetwork,
-  setVideoResolution,
-  setDeviceAudio,
-  launchApp,
-  setDiscoveryPolicy,
-  setLifecycleState,
-  unloadApp,
-  setCountryCode,
-  setLanguage,
-  setPreferredAudioLanguages,
-  setLocale,
-  setAdditionalInfo,
+/**
+ * Sets closed captions settings.
+ * @param {string} attribute - The attribute to set.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>}A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setClosedCaptions("setEnabled", "true");
+ */
+const setClosedCaptions = async (attribute, value) => {
+  return setterNotImplemented();
 };
+
+// TODO: Check whether it needs both attribute and value
+
+/**
+ * Sets the discovery policy.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setDiscoveryPolicy("Allow");
+ */
+const setDiscoveryPolicy = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Sets device audio.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setDeviceAudio("stereo");
+ */
+const setDeviceAudio = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Sets the device HDCP settings.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setDeviceHdcp("{"hdcp2.2": true}")
+ */
+const setDeviceHdcp = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Sets the device HDR settings.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setDeviceHdr("{"dolbyVision": true}")
+ */
+const setDeviceHdr = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Sets the device network settings.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setDeviceNetwork("{"state": "connected", "type": "wifi"}")
+ */
+const setDeviceNetwork = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Sets the language.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setLanguage("en-US");
+ */
+const setLanguage = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Sets the lifecycle state.
+ * @param {string} attribute - The attribute to set.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setLifecycleState("foreground");
+ */
+const setLifecycleState = async (value) => {
+  return setterNotImplemented();
+};
+
+// TODO: Check whether it needs both attribute and value
+
+/**
+ * Sets the limit ad tracking preference.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setLimitAdTracking("true");
+ */
+const setLimitAdTracking = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Sets the locale.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setLocale("en-US");
+ */
+const setLocale = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Sets the preferred audio languages.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setPreferredAudioLanguages(["en-US"]);
+ */
+const setPreferredAudioLanguages = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Sets the video resolution.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setVideoResolution("1080p");
+ */
+const setVideoResolution = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Sets voice guidance.
+ * @param {string} attribute - The attribute to set.
+ * @param {any} value - The value to set.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.setVoiceGuidance("enabled", "true");
+ */
+const setVoiceGuidance = async (attribute, value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * Unloads an application.
+ * @param {any} value - The application to unload.
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.unloadApp("AppName");
+ */
+const unloadApp = async (value) => {
+  return setterNotImplemented();
+};
+
+/**
+ * To get the current HDCP negotiation settings.
+ * @param {}
+ * @returns {Promise<any>} A promise that resolves/reject based on the response when the operation is complete.
+ * @example fcsSetters.getNegotiatedHdcp();
+ */
+const getNegotiatedHdcp = async () => {
+  return setterNotImplemented();
+};
+
+const fcsSetters = {
+  setAdditionalInfo,
+  setAudioDescriptionSettings,
+  setCountryCode,
+  setClosedCaptions,
+  setDiscoveryPolicy,
+  setDeviceAudio,
+  setDeviceHdcp,
+  setDeviceHdr,
+  setDeviceNetwork,
+  setLanguage,
+  launchApp,
+  setLifecycleState,
+  setLimitAdTracking,
+  setLocale,
+  setPreferredAudioLanguages,
+  setVideoResolution,
+  setVoiceGuidance,
+  unloadApp,
+  getNegotiatedHdcp,
+};
+
 module.exports = fcsSetters;
