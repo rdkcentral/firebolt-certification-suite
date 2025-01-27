4352603c186eb5aa771427dc31439dcde40bf9a2
Kandasamy, Divya <kdivya153@gmail.com>
2025-01-27 15:36:13 -0500

    Validating response in globalSetter

--- cypress/support/cypress-support/src/globalSetter.js
+++ cypress/support/cypress-support/src/globalSetter.js
@@ -1,10 +1,15 @@
 const UTILS = require('./utils');
 const CONSTANTS = require('../../constants/constants');
+import { apiObject } from '../../appObjectConfigs.js';
 
-global.setterSuccess = (message = 'Setter Method is success') => {
+global.setterSuccess = async (message = 'Setter Method is success') => {
   const methodName = UTILS.getEnvVariable(CONSTANTS.REQUEST_OVERRIDE_METHOD);
+  const params = UTILS.getEnvVariable(CONSTANTS.REQUEST_OVERRIDE_PARAMS);
+  const response = { jsonrpc: '2.0', result: null, id: 0 };
+  //Validating the response
+  await validateResponse(methodName, params, response);
   console.log(`[${methodName}] ${message}`);
-  return { jsonrpc: '2.0', result: null, id: 0 };
+  return response;
 };
 
 global.setterFailure = (message, error) => {
@@ -28,7 +33,32 @@ global.setterNotImplemented = (message) => {
     message || `Setter Method fcsSetters.${methodName} does not have an implementation`
   }`;
   const docLink = `Please see the fcsSetters.${methodName} documentation for implementation details: https://github.com/rdkcentral/firebolt-certification-suite/blob/main/defaultModule/requestModules/fcsSetters.md#${methodName}`;
-  const errorMessage = `${userMessage}\n${docLink}`;
+  const errorMessage = `NotSupported :: ${userMessage}\n${docLink}`;
   // Return the error message for proper rejection handling
   return new Error(errorMessage);
+};
+
+/**
+ * Validates the response and push API object to the global list.
+ * @param {object} methodName - Method Name to be validated .
+ * @param {object} params - The corresponding parameters for method.
+ *  @param {string} response - The response object.
+ */
+
+const validateResponse = async (methodName, params, response) => {
+  const appId = UTILS.fetchAppIdentifierFromEnv(CONSTANTS.FIRST_PARTY_APP);
+  const setterMethod = UTILS.getEnvVariable(CONSTANTS.FCS_SETTER_REQUEST_OVERRIDE_METHOD);
+  cy.updateResponseForFCS(methodName, params, response).then((updatedResponse) => {
+    const apiOrEventAppObject = new apiObject(
+      setterMethod, // Use the dynamically retrieved function name
+      params,
+      {},
+      updatedResponse,
+      CONSTANTS.RESULT,
+      appId
+    );
+    // Add the API object to the global list
+    const globalList = CONSTANTS.GLOBAL_API_OBJECT_LIST;
+    UTILS.getEnvVariable(globalList).push(apiOrEventAppObject);
+  });
 };
--- cypress/support/cypress-support/src/main.js
+++ cypress/support/cypress-support/src/main.js
@@ -254,9 +254,11 @@ export default function (module) {
     cy.wrap(requestMap, { timeout: CONSTANTS.SEVEN_SECONDS_TIMEOUT }).then(async (requestMap) => {
       return new Promise(async (resolve, reject) => {
         const [moduleName, methodName] = requestMap.method.split('.');
-        Cypress.env(CONSTANTS.REQUEST_OVERRIDE_METHOD, methodName);
+        Cypress.env(CONSTANTS.REQUEST_OVERRIDE_METHOD, requestMap.method);
+        Cypress.env(CONSTANTS.REQUEST_OVERRIDE_PARAMS, requestMap.params);
         // Check if request is for FCS setters
         if (moduleName === CONSTANTS.FCS_SETTER) {
+          Cypress.env(CONSTANTS.FCS_SETTER_REQUEST_OVERRIDE_METHOD, requestMap.method);
           const method = config.getRequestOverride(moduleName, methodName);
           if (typeof method === 'function') {
             const params = requestMap.params || {};
