e59803a229dbc7d9202aa7e82242f32eda0ff494
kdivya153 <kdivya153@gmail.com>
2025-04-03 18:37:00 -0400

    updated sdkVer

--- cypress/support/cypress-commands/commands.js
+++ cypress/support/cypress-commands/commands.js
@@ -273,11 +273,6 @@ Cypress.Commands.add('updateRunInfo', () => {
                 );
               Cypress.env(CONSTANTS.ENV_FIREBOLT_VERSION, fireboltVersion);
             }
-            if (!Cypress.env(CONSTANTS.ENV_RELEASE) && response?.debug) {
-              const release = response.debug;
-              Cypress.env(CONSTANTS.ENV_RELEASE, release);
-            }
-          } else if (deviceType.includes(CONSTANTS.DEVICE_ID)) {
             if (!Cypress.env(CONSTANTS.ENV_SDK_VERSION) && response?.sdk?.readable) {
               sdkVersion =
                 `${response?.sdk?.major}.${response?.sdk?.minor}.${response?.sdk?.patch}`.replace(
@@ -285,6 +280,10 @@ Cypress.Commands.add('updateRunInfo', () => {
                   ''
                 );
               Cypress.env(CONSTANTS.ENV_SDK_VERSION, sdkVersion);
+            }
+            if (!Cypress.env(CONSTANTS.ENV_RELEASE) && response?.debug) {
+              const release = response.debug;
+              Cypress.env(CONSTANTS.ENV_RELEASE, release);
             }
           } else {
             // Set environment variable with the response
@@ -371,7 +370,12 @@ Cypress.Commands.add('updateRunInfo', () => {
                   Cypress.env(CONSTANTS.ENV_SDK_VERSION) == 'N/A' ||
                   !Cypress.env(CONSTANTS.ENV_SDK_VERSION)
                 ) {
-                  return setEnvRunInfo(sdkVersion, CONSTANTS.DEVICE_ID, CONSTANTS.ACTION_CORE, {});
+                  return setEnvRunInfo(
+                    sdkVersion,
+                    CONSTANTS.DEVICE_VERSION,
+                    CONSTANTS.ACTION_CORE,
+                    {}
+                  );
                 }
               })
               .then(() => delay(2000))
