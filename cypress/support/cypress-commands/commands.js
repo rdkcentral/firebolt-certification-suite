3170b979c7dc09a21fc6f9c43cd047ef7875371e
preethi.m <preethi.m4@tataelxsi.co.in>
2025-04-22 16:09:13 +0530

    glue code changes

--- cypress/support/cypress-commands/commands.js
+++ cypress/support/cypress-commands/commands.js
@@ -2019,10 +2019,13 @@ const shouldPerformValidation = (key, value) => {
  * @example
  * cy.findLogPattern({ logPattern: "SignIn", fileName: "/logs/app.log" })
  */
-Cypress.Commands.add('findLogPattern', (param) => {
+Cypress.Commands.add('findLogPattern', (logKey, fileName) => {
   const requestMap = {
     method: CONSTANTS.REQUEST_OVERRIDE_CALLS.FINDLOGPATTERN,
-    params: param,
+    params: {
+      logPattern: logKey,
+      fileName: fileName
+    },
   };
   cy.sendMessagetoPlatforms(requestMap).then((result) => {
     return result;
