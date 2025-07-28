5ab8dc8b55ce3ee7d4a33f6623a77b6fed819f8c
Rajashekaravm <115162515+Rajashekaravm@users.noreply.github.com>
2025-07-28 12:40:52 +0530

    fix: Invalid/error AS response fix (#459)

--- cypress/support/cypress-commands/commands.js
+++ cypress/support/cypress-commands/commands.js
@@ -1213,6 +1213,8 @@ Cypress.Commands.add('launchApp', (appType, appCallSign, deviceIdentifier, inten
         });
         cy.getCapabilities();
         cy.updateRunInfo();
+      } else if (result && result.error) {
+        fireLog.fail(`App launch failed: ${result.error.message}`);
       }
     });
   });
